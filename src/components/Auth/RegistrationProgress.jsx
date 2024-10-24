53dc1aa944afb1c89b45774d0cd65c33979c4795
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-24 09:23:08 -1100

    Update RegistrationProgress.jsx

--- src/components/Auth/RegistrationProgress.jsx
+++ src/components/Auth/RegistrationProgress.jsx
@@ -1,36 +1,49 @@
 import React from "react";
 import { registration_steps_keys } from "../../utils/constants";
 
-function RegistrationProgress({state, dispatch}) {
+function RegistrationProgress({ state, dispatch }) {
   const getSteps = () => {
     return Object.keys(registration_steps_keys).map((key) => {
       const currentStep = registration_steps_keys[key];
 
-      const isSelectecd =  (currentStep.title === state.title) ? true : false
-      const icon = isSelectecd ? currentStep.activeIcon : currentStep.inactiveIcon
+      const isSelected = currentStep.title === state.title;
+      const icon = isSelected ? currentStep.activeIcon : currentStep.inactiveIcon;
 
       return (
-        <li key={currentStep.title} className={`w-full flex `}>
+        <li
+          key={currentStep.title}
+          className="w-full flex sm:flex-col flex-row items-center"
+        >
           <div className="flex flex-col items-center w-[25%]">
-            <div className={`bg-white rounded-md p-[15px] ${isSelectecd && 'border-2 border-gray-800 transition duration-1000'}`}>
-              <img className="h-[25px] w-[25px]  " src={icon} />
+            <div
+              className={`bg-white rounded-md p-[15px] ${
+                isSelected && "border-2 border-gray-800 transition duration-1000"
+              }`}
+            >
+              <img className="h-[25px] w-[25px]" src={icon} />
             </div>
-            {currentStep.title !== registration_steps_keys.welcome_video.title && <hr className="h-[25px] border-dashed border w-0" />}
+            {currentStep.title !==
+              registration_steps_keys.welcome_video.title && (
+              <hr className="h-[25px] border-dashed border w-0" />
+            )}
           </div>
 
-          <div>
-            <p className="font-semibold text-[15px] text-white">{currentStep.title}</p>
-            <p className="font-meduim text-small text-gray-400">{currentStep.desc}</p>
+          <div className="flex-1">
+            <p className="font-semibold text-[15px] text-white">
+              {currentStep.title}
+            </p>
+            <p className="font-medium text-small text-gray-400">
+              {currentStep.desc}
+            </p>
           </div>
         </li>
       );
     });
   };
 
-
   return (
     <div className="w-full h-full bg-green flex items-center justify-center">
-      <ul className="flex flex-row sm:flex-col w-full sm:w-[60%]">{getSteps()}</ul>
+      <ul className="w-full sm:w-[60%]">{getSteps()}</ul>
     </div>
   );
 }
