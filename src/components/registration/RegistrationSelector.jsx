ccc70713da9149ed9192d40b313ca152221dc44c
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-24 07:07:56 -1100

    Update RegistrationSelector.jsx

--- src/components/registration/RegistrationSelector.jsx
+++ src/components/registration/RegistrationSelector.jsx
@@ -1,8 +1,8 @@
 import { Fragment, useState } from 'react';
-import { Combobox, Transition, } from '@headlessui/react';
+import { Combobox, Transition } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
-function RegistrationSelector({ data, selected, setSelected, icon}) {
+function RegistrationSelector({ data, selected, setSelected, icon }) {
   const [query, setQuery] = useState('');
 
   const filteredData =
@@ -15,32 +15,20 @@ function RegistrationSelector({ data, selected, setSelected, icon}) {
             .includes(query.toLowerCase().replace(/\s+/g, ''))
         );
 
-  const returnValue = (val) => {
-
-      return val.name;
-    
-  };
-
   return (
-    <div className="w-full ">
-      <Combobox
-        value={selected}
-        onChange={(val) => {
-          setSelected(val);
-        }
-      }
-      >
-        <div className="relative mt-1 border rounded-md  ">
-          <div className="relative w-full flex items-center h-[40px] pl-[10px]  cursor-default overflow-hidden rounded-lg bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-          <img src={icon} className="w-[20px]" />
+    <div className="w-full">
+      <Combobox value={selected} onChange={setSelected}>
+        <div className="relative mt-1 border rounded-md">
+          <div className="relative flex items-center h-[40px] px-2 sm:px-4 cursor-default overflow-hidden rounded-lg bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-300 focus-visible:ring-offset-2 sm:text-sm">
+            <img src={icon} className="w-[20px] h-[20px] object-contain" />
             <Combobox.Input
-              className="w-full outline-none border-none py-[6px] pl-3 pr-10 text-sm leading-5 font-normal text-gray-400 focus:ring-0"
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-600 focus:ring-0 outline-none"
               displayValue={(current) => current.name}
               onChange={(event) => setQuery(event.target.value)}
             />
-            <Combobox.Button className="absolute inset-y-0 right-0 w-[25px] justify-center  flex items-center pr-2">
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
-                className="h-full w-full text-gray-400 hover:text-gray-500"
+                className="h-5 w-5 text-gray-400 hover:text-gray-500"
                 aria-hidden="true"
               />
             </Combobox.Button>
@@ -52,17 +40,17 @@ function RegistrationSelector({ data, selected, setSelected, icon}) {
             leaveTo="opacity-0"
             afterLeave={() => setQuery('')}
           >
-            <Combobox.Options className="absolute mt-1 z-10 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {filteredData?.length === 0 && query !== '' ? (
+            <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredData.length === 0 && query !== '' ? (
                 <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                   Nothing found.
                 </div>
               ) : (
-                filteredData?.map((current) => (
+                filteredData.map((current) => (
                   <Combobox.Option
                     key={current.id}
                     className={({ active }) =>
-                      `relative cursor-default select-none flex py-2 pl-10 pr-4 ${
+                      `relative cursor-default select-none flex items-center py-2 pl-10 pr-4 ${
                         active ? 'bg-green/70 text-white' : 'text-gray-900'
                       }`
                     }
@@ -70,23 +58,22 @@ function RegistrationSelector({ data, selected, setSelected, icon}) {
                   >
                     {({ selected, active }) => (
                       <>
-                      {selected ? (
+                        {selected && (
                           <span
-                            className={`absolute inset-y-0 left-[-30px] flex items-center  ${
+                            className={`absolute inset-y-0 left-2 flex items-center ${
                               active ? 'text-white' : 'text-teal-600'
                             }`}
                           >
                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
                           </span>
-                        ) : null}
+                        )}
                         <span
                           className={`block truncate ${
                             selected ? 'font-medium' : 'font-normal'
                           }`}
                         >
-                          {current.name ? current.name : 'No name Attached'}
+                          {current.name || 'No name Attached'}
                         </span>
-                        
                       </>
                     )}
                   </Combobox.Option>
