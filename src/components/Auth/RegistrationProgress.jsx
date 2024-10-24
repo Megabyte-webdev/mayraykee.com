5c49326d50b2bcfff012ba8aa582e4743c8a571c
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-24 08:08:55 -1100

    Update RegistrationProgress.jsx

--- src/components/Auth/RegistrationProgress.jsx
+++ src/components/Auth/RegistrationProgress.jsx
@@ -36,7 +36,7 @@ function RegistrationProgress({ state, dispatch }) {
 
   return (
     <div className="w-full sm:w-full h-full bg-green flex items-center justify-center">
-      <ul className="w-max sm:w-[60%] flex flex-col gap-3 sm:gap-6">{getSteps()}</ul>
+      <ul className="w-max sm:w-[60%] flex flex-row sm:flex-col gap-3 sm:gap-6">{getSteps()}</ul>
     </div>
   );
 }
