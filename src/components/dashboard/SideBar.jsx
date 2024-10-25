ca6cab457b437a95643b2a61e0dd8b97e89ca29d
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-25 07:03:41 -1100

    Update SideBar.jsx

--- src/components/dashboard/SideBar.jsx
+++ src/components/dashboard/SideBar.jsx
@@ -14,13 +14,7 @@ function SideBar({children}) {
 
       <InviteComponent/>
     </section>
- <section className="md:hidden fixed left-[-100%] w-[16%] relative h-full flex justify-start flex-col items-center gap-[5%]  py-[0.8%]">
-      <img className="w-[90%] " src={MainLogo} />
 
-      <ul className="flex flex-col w-full pl-[2%] items-center gap-[5%] h-[80%]">{children}</ul>
-
-      <InviteComponent/>
-    </section>
 </>
   );
 }
