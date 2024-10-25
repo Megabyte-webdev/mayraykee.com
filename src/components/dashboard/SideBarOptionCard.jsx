6583e779424f136cd1086c8b830dedd38d456aca
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-25 05:59:14 -1100

    Update SideBarOptionCard.jsx

--- src/components/dashboard/SideBarOptionCard.jsx
+++ src/components/dashboard/SideBarOptionCard.jsx
@@ -13,7 +13,7 @@ function SideBarOptionCard({option, state, dispatch}) {
     return (
         <li onClick={handleOnClick} className={`flex w-[80%] cursor-pointer   duration-100 h-[8%] justify-start gap-[10%] pl-[10px] p-[5px] items-center ${state.title === option.title ? 'bg-green text-white shadow-lg font-semibold rounded-[15px]' : 'hover:scale-105'}`}>
             <img className='h-[25px]' src={state.title === option.title ? option.activeIcon : option.inactiveIcon}/>
-            <span className='bg-black'>{option.title}</span>
+            <span className='hidden md:block'>{option.title}</span>
         </li>
     )
 }
