724fc5b0d9ca150d36b45cfb257e850d53a335e1
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-25 07:51:12 -1100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,10 +10,7 @@ import LineChart from "../components/dashboard/chart/LineChart";
 import ProgressPieChart from "../components/dashboard/chart/ProgressPieChart";
 import { Helmet } from "react-helmet";
 
-const preselectedDates = [
-  new Date(2024, 5, 15),
-  new Date(2024, 5, 16),
-];
+const preselectedDates = [new Date(2024, 5, 15), new Date(2024, 5, 16)];
 
 function Dashboard() {
   const [date, setDate] = useState(null);
@@ -24,45 +21,47 @@ function Dashboard() {
   }, [date]);
 
   return (
-    <div className="flex flex-col gap-4 md:gap-0 md:flex-row w-full h-full px-4 md:px-[2%] pt-[1%]">
+    <div className="flex flex-col md:flex-row w-full h-full px-4 md:px-[2%] pt-[1%] overflow-hidden">
       <Helmet>
         <title>Mayrahkee - Africa | Dashboard</title>
       </Helmet>
 
-      <section className="w-full md:w-[70%] flex flex-col gap-6 md:gap-[5%] pt-[3%]">
-        <ul className="grid gap-4 md:grid-cols-2">
-          <div className="h-[190px] w-[200px] md:w-[350px] rounded-[10px] bg-red-100 overflow-hidden cursor-pointer hover:scale-105 duration-150">
+      {/* Left Section */}
+      <section className="w-full md:w-[70%] flex flex-col gap-6 pt-[3%]">
+        <ul className="grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
+          <li className="h-[190px] max-w-full md:w-[350px] rounded-[10px] bg-red-100 overflow-hidden cursor-pointer hover:scale-105 duration-150">
             <img
               src={BoardMen}
               alt="Board Men"
               className="h-full w-full object-cover"
             />
-          </div>
+          </li>
 
-          <div className="w-full md:w-[350px] cursor-pointer group hover:scale-105 duration-150 flex flex-col gap-2 justify-center items-center rounded-[10px] overflow-hidden h-[190px] bg-cover bg-white">
+          <li className="h-[190px] max-w-full md:w-[350px] bg-white rounded-[10px] overflow-hidden flex flex-col items-center justify-center cursor-pointer group hover:scale-105 duration-150">
             <img className="w-[80px] md:w-[100px]" src={Computer} alt="" />
             <span className="text-green text-sm">Enter Class Room</span>
-          </div>
+          </li>
 
-          <div className="w-full md:w-[350px] cursor-pointer hover:scale-105 duration-150 flex flex-col gap-2 justify-center items-center rounded-[10px] overflow-hidden h-[190px] bg-cover bg-customBrown">
+          <li className="h-[190px] max-w-full md:w-[350px] bg-customBrown rounded-[10px] overflow-hidden flex flex-col items-center justify-center cursor-pointer hover:scale-105 duration-150">
             <img className="w-[80px] md:w-[100px]" src={Dialog} alt="" />
             <span className="text-white text-sm">Forum</span>
-          </div>
+          </li>
 
-          <div className="w-full md:w-[350px] cursor-pointer hover:scale-105 duration-150 flex flex-col gap-2 justify-center items-center rounded-[10px] overflow-hidden h-[190px] bg-cover bg-green">
+          <li className="h-[190px] max-w-full md:w-[350px] bg-green rounded-[10px] overflow-hidden flex flex-col items-center justify-center cursor-pointer hover:scale-105 duration-150">
             <img className="w-[80px] md:w-[90px]" src={Calenderr} alt="" />
             <span className="text-white text-sm">
               Course Schedule / Time Table
             </span>
-          </div>
+          </li>
         </ul>
 
-        <div className="flex flex-wrap w-full justify-between pr-0 md:pr-[8%] min-h-[100px] gap-4">
+        <div className="flex flex-wrap w-full gap-4 min-h-[100px] justify-between">
           <LineChart />
           <ProgressPieChart />
         </div>
       </section>
 
+      {/* Right Section */}
       <section className="w-full md:w-[30%] flex flex-col gap-6">
         <div className="bg-gray-400 h-[300px] md:h-[55%] w-full rounded-[10px]">
           <Calendar
