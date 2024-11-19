a00ad95c723600aa715fc2470f2e1310ed6129e5
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-19 10:17:06 +0100

    Update ChatList.jsx

--- src/components/instant-chat/ChatList.jsx
+++ src/components/instant-chat/ChatList.jsx
@@ -13,7 +13,7 @@ function ChatList({selectedUser,setSelectedUser}) {
 
   return (
     <div className="h-max w-full md:w-1/4 flex flex-col items-center gap-[3%] shadow-sm shadow-gray-300 py-[1%]  bg-white">
-        <h3 className="font-semibold text-start w-full text-md px-[7.5%] text-gray-500">Chat</h3>
+        <h3 className="font-semibold text-start w-full text-md px-[7.5%] text-gray-500">openDetails && <p className="block md:hidden" onClick={()=>setOpenDetails (true)}>open</p>    Chat</h3>
         <button className="flex text-small gap-[5px] justify-center  hover:scale-105 duration-75 items-center text-white rounded-[30px] w-[85%] bg-green px-[5px] py-[5px]"><FaPlus/> New Conversation</button>
        <ul className="min-h-20 w-full flex flex-row md:flex-col py-2 overflow-x-auto overflow-y-hidden md:overflow-y-auto">
           {getUsers()}
