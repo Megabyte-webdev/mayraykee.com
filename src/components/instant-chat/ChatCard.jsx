3852cb0e5c5b0d88c8feb6300494c0d0c88ad95c
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-19 23:12:25 +0100

    Update ChatCard.jsx

--- src/components/instant-chat/ChatCard.jsx
+++ src/components/instant-chat/ChatCard.jsx
@@ -2,13 +2,13 @@ function ChatCard({ currentChat, selectedUser, setSelectedUser }) {
   return (
     <div
       onClick={() => setSelectedUser(currentChat)}
-      className={`w-full flex flex-col md: flex-row justify-center md:justify-start ${
+      className={`w-full flex justify-center md:justify-start ${
         currentChat.id === selectedUser?.id
           ? 'bg-gray-100 md:border-l-[3px] md:border-l-green md:border-b-0 border-b-[3px] border-b-green'
           : 'md:border-l-0 border-b border-b-gray-400'
       } px-[7.5%] items-center pb-[10px] cursor-pointer hover:bg-gray-100`}
     >
-      <div className="flex items-center gap-[10px] py-[10px]">
+      <div className="flex flex-col md: flex-row items-center gap-[10px] py-[10px]">
         <div className="w-16 h-16 md:h-20 md:w-20 rounded-full overflow-hidden">
           <img
             src={currentChat.profilePic}
