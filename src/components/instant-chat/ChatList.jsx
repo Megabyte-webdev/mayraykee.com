5c69ed1316f59bdd41af7bf93cd15c22c66e408c
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-19 23:22:39 +0100

    Update ChatList.jsx

--- src/components/instant-chat/ChatList.jsx
+++ src/components/instant-chat/ChatList.jsx
@@ -12,7 +12,7 @@ function ChatList({selectedUser,setSelectedUser}) {
     }
 
   return (
-    <div className="flex-1 h-max md:h-full w-full flex flex-col items-center gap-[3%] shadow-sm shadow-gray-300 py-[1%] bg-white">
+    <div className="sticky top-0 flex-1 h-max md:h-full w-full flex flex-col items-center gap-[3%] shadow-sm shadow-gray-300 py-[1%] bg-white">
         <h3 className="font-semibold text-start w-full text-md px-[7.5%] text-gray-500 my-2"> Chat</h3>
         <button className="flex text-small gap-[5px] justify-center hover:scale-105 duration-75 items-center text-white rounded-[30px] w-[85%] bg-green px-[5px] py-[7px]"><FaPlus/> New Conversation</button>
        <ul className="min-h-20 w-full flex md:justify-start gap-2 min-[900px]:flex-col py-2 overflow-x-auto overflow-y-hidden md:overflow-y-auto">
