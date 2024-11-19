948ec302ae194cfc601628358cefeedd0ef82346
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-19 09:40:41 +0100

    Update InstantChat.jsx

--- src/pages/InstantChat.jsx
+++ src/pages/InstantChat.jsx
@@ -7,7 +7,7 @@ import ConversationBox from "../components/instant-chat/ConversationBox";
 function InstantChat() {
     const [selectedUser,setSelectedUser] = useState()
   return (
-    <div className="w-full h-full px-[2%] py-[1%] justify-start gap-[1%] flex">
+    <div className="w-full h-full px-[2%] py-[1%] justify-start gap-[1%] flex flex-col md:flex-row">
       <ChatList selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
       <ConversationBox currentUser={currentUser} selectedUser={selectedUser}/>
       <CurrentChatDetails selectedUser={selectedUser}/>
