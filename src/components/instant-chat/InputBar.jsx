b2e0ae669a748789725c8315cd2d66cb734a9b28
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-19 22:42:48 +0100

    Update InputBar.jsx

--- src/components/instant-chat/InputBar.jsx
+++ src/components/instant-chat/InputBar.jsx
@@ -2,17 +2,16 @@ import { FaPlus } from "react-icons/fa6";
 
 function InputBar() {
   return (
-    <div className="absolute bottom-0 left-0 w-full flex px-[3%] items-center gap-[3%] rounded-[30px] border border-gray-300 py-[5px] bg-white">
+    <div className="w-full flex px-[3%] items-center gap-[3%] sticky rounded-[30px] border border-gray-300 py-[5px] bg-white bottom-0">
       <FaPlus className="text-[14px] text-gray-800 cursor-pointer" />
 
       <input
         className="w-[80%] focus:outline-none text-small text-center"
         placeholder="Type Something...."
       />
-      <button className="w-[20%] bg-green text-white h-[30px] rounded-[30px] hover:scale-105 duration-75">
-        Send
-      </button>
+      <button className="w-[20%] bg-green text-white h-[30px] rounded-[30px] hover:scale-105 duration-75">Send</button>
     </div>
   );
 }
+
 export default InputBar;
