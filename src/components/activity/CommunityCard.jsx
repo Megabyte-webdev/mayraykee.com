05966c53ce07b29d60872450b447a62482e37d93
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-20 10:18:08 +0100

    Update CommunityCard.jsx

--- src/components/activity/CommunityCard.jsx
+++ src/components/activity/CommunityCard.jsx
@@ -1,6 +1,6 @@
 import PeopleSitting from '../../assets/pngs/people-sitting.png'
 function CommunityCard() {
-    return ( <div className="w-full md:w-[300px] h-[30%] flex flex-col bg-white rounded-[10px]">
+    return ( <div className="w-full md:w-1/3 h-[30%] flex flex-col bg-white rounded-[10px]">
         <img src={PeopleSitting} className=''/>
         <div className='px-[5%]'>
         <h3 className='text-sm text-gray-500'>Details</h3>
