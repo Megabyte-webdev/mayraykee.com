7ecf4542035f6053c9b88f9d17f8e1b4fb2af9f2
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-20 01:27:22 +0100

    Update MeetingTimeEvent.jsx

--- src/components/activity/MeetingTimeEvent.jsx
+++ src/components/activity/MeetingTimeEvent.jsx
@@ -21,7 +21,7 @@ function MeetingTimeEvent({ currentEvent }) {
 
   return (
     <li className="w-full flex gap-[25px] mb-[2%] last:has-[hr]:h-0 last:min-h-fit min-h-[50px] ">
-      <div className="flex flex-col w-[10%] h-full">
+      <div className="flex flex-col h-full">
         <span className="text-gray-400 text-small place-self-center">
           {currentEvent.time}
         </span>
