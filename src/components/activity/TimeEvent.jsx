2c536c3eeb6386dc407d91f19ccc1b2832cb4a1f
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-20 13:31:21 +0100

    Update TimeEvent.jsx

--- src/components/activity/TimeEvent.jsx
+++ src/components/activity/TimeEvent.jsx
@@ -34,7 +34,7 @@ function TimeEvent({ currentEvent }) {
 
   return (
     <li className="w-full flex gap-[25px] last:has-[hr]:h-0 last:min-h-fit min-h-[50px]">
-      <div className="flex flex-col min-w-60 h-full">
+      <div className="flex flex-col w-max h-full">
         <span className="text-gray-400 text-small place-self-center">
           {currentEvent.time}
         </span>
