996f53becf600b87f4ffcba8a35e2847be9c53c3
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-12 06:33:09 -1100

    Update PopularCourses.jsx

--- src/components/courses/PopularCourses.jsx
+++ src/components/courses/PopularCourses.jsx
@@ -17,7 +17,7 @@ function PopularCourses({popular}) {
           <button className="text-sm flex items-center gap-[5px] hover:scale-105 duration-105 text-green">View all<img className="h-[10px]" src={ArrowRight}/></button>
         </div>
 
-        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-full justify-around gap-2'>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-full justify-around place-items-center gap-2'>
              {getCourses()}
         </ul>
 
