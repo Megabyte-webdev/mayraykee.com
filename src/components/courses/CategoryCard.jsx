c9b90d6ea1bf0f8a308c4b5bdac79271776a7eb5
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-12 06:36:52 -1100

    Update CategoryCard.jsx

--- src/components/courses/CategoryCard.jsx
+++ src/components/courses/CategoryCard.jsx
@@ -7,7 +7,7 @@ function CategoryCard({data}) {
     const navigate = useNavigate();
     
     return ( 
-    <li onClick={()=>navigate(`/dashboard/courses/preview/${data.id} ${data?.title}`, {state: {data: data, previousPath: location.pathname}})} className="px-[10px] min-w-40 min-h-20 group cursor-pointer hover:scale-105 duration-100 hover:shadow-md py-[5px] justify-between bg-white items-center rounded-[10px] flex gap-[2px]">
+    <li onClick={()=>navigate(`/dashboard/courses/preview/${data.id} ${data?.title}`, {state: {data: data, previousPath: location.pathname}})} className="px-[10px] w-full sm:min-w-40 min-h-20 group cursor-pointer hover:scale-105 duration-100 hover:shadow-md py-[5px] justify-between bg-white items-center rounded-[10px] flex gap-[2px]">
         <img className="h-[80%]" src={data?.image || ColorPallete}/>
         <div className="flex-1 flex flex-col gap-[5px]">
             <span className="text-sm font-semibold text-gray-500">{data?.title}</span>
